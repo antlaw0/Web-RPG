d2ff43f28b9dc924af97cbfc6e0170ae721c4dff
AAA <aaa@aaa.com>
2017-10-17 16:46:36 -0500

    works

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def robots():
     return res
 
 #if __name__ == '__main__':
-port = int(os.environ.get('PORT', 5000))
-app.run(host='127.0.0.1', port=port, debug=True)
+#port = int(os.environ.get('PORT', 5000))
+#app.run(host='127.0.0.1', port=port, debug=True)
