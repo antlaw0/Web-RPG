af3082135667ecddc4748fea778e1856293e5884
AAA <aaa@aaa.com>
2017-10-22 17:25:02 -0500

    test

--- app.py
+++ app.py
@@ -24,12 +24,11 @@ class User(db.Model):
 
 @app.route('/')
 def home():
-	#user = User('John Doe', 'john.doe@example.com')
-	#db.session.add(user)
-	#db.session.commit()
-	#all_users = User.query.all()
-	name="Test"
-	#name= all_users[0].name
+	user = User('John Doe', 'john.doe@example.com')
+	db.session.add(user)
+	db.session.commit()
+	all_users = User.query.all()
+	name= all_users[0].name
 	return render_template('index.html', name=name)
 
 
