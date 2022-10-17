116e81b24807757f433ce6c5e9a834bcb692b92d
antlaw0 <antlaw0@gmail.com>
2022-10-17 11:47:04 -0500

    Inital commit

--- app.py
+++ app.py
@@ -133,6 +133,6 @@ def command():
     #return jsonify(output)
     return render_template('game.html', output=output)
 
-#if __name__ == '__main__':
-#port = int(os.environ.get('PORT', 5000))
-#app.run(host='127.0.0.1', port=port, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port, debug=True)
